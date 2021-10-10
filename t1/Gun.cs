23cf9b08f2fc9006e47e89d641cd898341caead8
khanisha123 <khanisha@code.edu.az>
2021-10-10 21:32:41 +0400

    Add project files.

---
+++ t1/Gun.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace t1_
+{
+    class Gun
+    {
+        private int bulletCount;
+        public int BulletCount 
+        {
+            get
+            {
+                return bulletCount;
+              
+
+                
+
+            } 
+            set 
+            {
+                bulletCount = value;
+                return;
+
+            } 
+        }
+
+        public int GunReloadOneByOne() 
+        {
+            if (bulletCount!=30)
+            {
+                bulletCount++;
+            }
+            else
+            {
+                Console.WriteLine("Magazine full");
+                
+            }
+            return bulletCount;
+
+        }
+        public int GunReload() 
+        {
+            if (bulletCount!=30)
+            {
+                bulletCount = 30;
+                
+            }
+            return bulletCount;
+
+        }
+        public int ShootingOneByOne() 
+        {
+            
+            if (bulletCount<=30 && bulletCount>0)
+            {
+                bulletCount--;
+            }
+            else
+            {
+                Console.WriteLine("You Havnt got a bullet in Magazine");
+            }
+            return bulletCount;
+
+        }
+        public int ShootingAuto() 
+        {
+            if (bulletCount<=30 && bulletCount>0 )
+            {
+                
+                bulletCount = 0;
+
+            }
+            else
+            {
+                Console.WriteLine("You Havnt got a bullet in Magazine");
+            }
+            return bulletCount;
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+   
+    
---
+++ t1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace t1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun obj1 = new Gun();
+            
+            obj1.BulletCount = 30;
+            if (obj1.BulletCount>30)
+            {
+                Console.WriteLine("eror");
+            }
+
+            Console.WriteLine($"GunReloadOneByOne = {obj1.GunReloadOneByOne()}");
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine(obj1.GunReloadOneByOne());
+            Console.WriteLine("      ");
+
+
+            Console.WriteLine($"GunReloadAuto = {obj1.GunReload()}");
+            Console.WriteLine("    ");
+
+
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine($"BulletCountShootingOneByOne = {obj1.ShootingOneByOne()}");
+            Console.WriteLine("      ");
+            Console.WriteLine($"AutoShooting = {obj1.ShootingAuto()}");
+
+
+
+
+
+
+
+
+        }
+    } 
+}
